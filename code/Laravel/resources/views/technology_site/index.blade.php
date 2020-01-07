13a7e2940d68ebeddcc2c23f7d6b5af77d242dc8
Croe2019 <sac77qbc@gmail.com>
2020-01-07 22:03:32 +0900

    背景画像追加、デザイン部分強化

--- code/Laravel/resources/views/technology_site/index.blade.php
+++ code/Laravel/resources/views/technology_site/index.blade.php
@@ -8,6 +8,7 @@
 <body>
     <div class="title">
         <h1>TechnologySite(仮)</h1>
+        <img class="img" src="{{ asset('image/WEB.jpg')}}" alt="">
     </div>
 </body>
 
@@ -19,6 +20,32 @@
            コードを書いていて気づいたことや、自分がハマったあの仕様について、
            他のエンジニアと知見を共有しましょう ;)
         </h2>
+    </div>
+
+    <div class="user_registration">
+        <!-- ここにユーザー登録画面に遷移するURLを設置する -->
+        <h1>ユーザー登録</h1>
+    </div>
+
+    <div class="login">
+        <!-- ここにログイン画面に遷移するURLを設置する -->
+        <h1>ログイン</h1>
+    </div>
+
+    <form action="" method="">
+        <div class="registration_form">
+          <label>ユーザー名</label> <input type="name" id="name" name="name" placeholder="siteman" class="user_name">
+
+          <label>メールアドレス</label> <input type="email" id="email" name="email" placeholder="site@gmail.com" class="email_address">
+          
+          <input type="submit" value="登録する">
+        </div>
+    </form>
+    
+
+    <div class="Trend">
+        <!-- ここに人気の記事一覧を表示し、記事のページに遷移できるようにする -->
+
     </div>
 </header>
     
